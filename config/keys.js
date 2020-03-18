cd5c38885728926840dc8efdaf1856e8ad4ab878
MicoLagang <lagang83@gmail.com>
2020-03-18 16:46:10 +0800

    part2 updated

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// add this file to .gitignore
+module.exports = {
+    google: {
+        clientID: '89145026859-iksl956pj5mlrfvfv51p11ohl8qlk0ok.apps.googleusercontent.com',
+        clientSecret: 'iY3G4Pt8Gh-DYwL5na9H-4lw'
+    }
+}
