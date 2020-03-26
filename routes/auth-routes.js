399cbdc5f9d68e5d1e34a26ebaecd82bb6063f3a
MicoLagang <lagang83@gmail.com>
2020-03-26 10:45:15 +0800

    part6 updated

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -9,7 +9,7 @@ router.get('/login',(req, res)=>{
 // auth logout
 router.get('/logout',(req, res)=>{
     // handle with passport
-    res.send('logging out');
+    res.render('logout');
 });
 
 // auth with google
